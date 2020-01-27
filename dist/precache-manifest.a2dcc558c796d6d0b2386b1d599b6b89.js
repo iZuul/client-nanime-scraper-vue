a0642b4df06b2378f583872124c6e687714e9ad6
iZuul <zdhiaulhaq20@gmail.com>
2020-01-27 11:47:28 +0700

    initial commit

---
+++ dist/precache-manifest.a2dcc558c796d6d0b2386b1d599b6b89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fead3c292b25b367aab64b21ff28e8c8",
+    "url": "/css/style.css"
+  },
+  {
+    "revision": "fbc8933359098de99f922b4eef8d537e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8c2c261793d677cf833",
+    "url": "/js/app.6e1e9b88.js"
+  },
+  {
+    "revision": "fe3b400a2d71585df94d",
+    "url": "/js/chunk-vendors.631e1d68.js"
+  },
+  {
+    "revision": "edae9c5904a315cad2c86aeabcf7c779",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
