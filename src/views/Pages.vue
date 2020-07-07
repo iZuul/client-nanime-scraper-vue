5ac759c6aa10ecf6d4b3fe9860102cf628d35ac7
zulfadhiaulhaq <zdhiaulhaq20@gmail.com>
2020-07-07 15:43:59 +0700

    update package.json

--- src/views/Pages.vue
+++ src/views/Pages.vue
@@ -1,73 +1,77 @@
 <template>
   <main class="container">
     <form id="searchForm" @submit="searchingAnime" action="/">
-        <div class="form-group">
-            <label class="col-form-label" for="search">Search</label>
-            <input type="text" class="form-control" placeholder="Search for anime...." id="search" v-model="search">
-        </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+      <div class="form-group">
+        <label class="col-form-label" for="search">Search</label>
+        <input
+          type="text"
+          class="form-control"
+          placeholder="Search for anime...."
+          id="search"
+          v-model="search"
+        />
+      </div>
+      <button type="submit" class="btn btn-primary">Submit</button>
     </form>
-    <Loading v-if="loading"/>
-    <ListAnimes/>
-    <Pagination/>
+    <Loading v-if="loading" />
+    <ListAnimes />
+    <Pagination />
   </main>
 </template>
 
 <script>
-import SearchBox from '../components/SearchBox.vue'
-import Loading from '../components/Loading.vue'
-import ListAnimes from '../components/ListAnimes.vue'
-import Pagination from '../components/Pagination.vue'
-import {mapState, mapGetters} from 'vuex'
+import SearchBox from "../components/SearchBox.vue";
+import Loading from "../components/Loading.vue";
+import ListAnimes from "../components/ListAnimes.vue";
+import Pagination from "../components/Pagination.vue";
+import { mapState, mapGetters } from "vuex";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
-      SearchBox,
-      Loading,
-      ListAnimes,
-      Pagination
+    SearchBox,
+    Loading,
+    ListAnimes,
+    Pagination
   },
-  computed:{
+  computed: {
     ...mapState({
-      loading:'loading',
-      anime_search:'anime_search'
+      loading: "loading",
+      anime_search: "anime_search"
     }),
-    ...mapGetters([
-      'getAnimeSearch'
-    ])
+    ...mapGetters(["getAnimeSearch"])
   },
   data() {
     return {
-      search:''
-    }
+      search: ""
+    };
   },
-  watch:{
-    '$route.params.page': function(val){
-      console.log('page change ' , this.$route.params.page)
-      if(this.$route.params.page === undefined){
-        console.log('undefined clicked')
-        this.$store.dispatch('retrieveAnimes', this.getAnimeSearch)
-      }else{
-        this.$store.dispatch('retrieveAnimesPerPage', {
+  watch: {
+    "$route.params.page": function(val) {
+      console.log("page change ", this.$route.params.page);
+      if (this.$route.params.page === undefined) {
+        console.log("undefined clicked");
+        this.$store.dispatch("retrieveAnimes", this.getAnimeSearch);
+      } else {
+        this.$store.dispatch("retrieveAnimesPerPage", {
           anime_name: this.anime_search,
           page: this.$route.params.page
-        })
+        });
       }
     }
   },
-  created(){
-    this.$store.dispatch('retrieveAnimesPerPage', {
+  created() {
+    this.$store.dispatch("retrieveAnimesPerPage", {
       anime_name: this.anime_search,
       page: this.$route.params.page
-    })
+    });
   },
-  mounted(){},
+  mounted() {},
   methods: {
     searchingAnime() {
-        console.log('clicked', this.search)
-        this.$store.dispatch('retrieveAnimes', this.search)
+      console.log("clicked", this.search);
+      this.$store.dispatch("retrieveAnimes", this.search);
     }
   }
-}
+};
 </script>
